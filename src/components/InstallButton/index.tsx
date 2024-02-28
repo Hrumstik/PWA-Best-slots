f12f2933b422d7c712763548f5419203f7edabaa
Alexander Gurinovich <hrumstik.warsaw@gmail.com>
2024-02-28 07:31:26 +0100

    fixed bug with event promt

--- src/components/InstallButton/index.tsx
+++ src/components/InstallButton/index.tsx
@@ -85,8 +85,9 @@ const InstallButton: React.FC<Props> = ({ appLink }) => {
         handleBeforeInstallPrompt as EventListener
       );
       window.removeEventListener("appinstalled", handleAppInstalled);
+      installPromptRef.current = null;
     };
-  }, [appLink, dispatch]);
+  }, []);
 
   const installPWA = async () => {
     trackEvent("landing_btn_install_pressed");
